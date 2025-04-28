7daac05337a41c079be4278d8e7f4d3e2b0e40cd
Phineas1500 <sriram.kiron@gmail.com>
2025-04-28 18:24:39 -0400

    reverted redirect changes

--- next.config.ts
+++ next.config.ts
@@ -4,21 +4,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  async redirects() {
-    return [
-      {
-        source: "/:path*", // Match anything
-        has: [
-          {
-            type: "host",
-            value: "www.minomize.com",
-          },
-        ],
-        destination: "https://minomize.com/:path*", // Redirect and keep the path
-        permanent: true, // 308 Permanent Redirect
-      },
-    ];
-  },
 };
 
 export default nextConfig;
